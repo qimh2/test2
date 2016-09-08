7045af25cb13252c88d52bc98ea71f296f229210
qimh2 <2237870348@qq.com>
2016-09-08 17:39:09 +0800

    添加servletPro2项目到仓库

---
+++ servletPro2/src/com/qimh/servlet/RequestTest.java
@@ -0,0 +1,43 @@
+package com.qimh.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RequestTest
+ */
+public class RequestTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		//指定页面不缓存
+		//response.setDateHeader("Expires", -1);//ie 禁止缓存
+		//response.setDateHeader("Expires", System.currentTimeMillis()+3600*1000);//缓存一个小时
+		//为了保证兼容性
+		//response.setHeader("Pragma", "no-cache");
+		//response.setHeader("Cache-Control", "no-cache");
+		
+		out.println("hello world2222"+new Date().toString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doGet(request, response);
+	}
+
+}
