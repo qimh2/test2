c43cdd968fe2d94cc5cf4f5b6639ef5d9e9af62f
qimh2 <2237870348@qq.com>
2016-09-08 12:41:53 +0800

    添加servletPro项目到仓库

---
+++ servletPro/src/com/qimh/servlet/DownLoadFile.java
@@ -0,0 +1,41 @@
+package com.qimh.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DownLoadFile
+ */
+public class DownLoadFile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+		String dir = "/files/";
+		String fileName = request.getParameter("fileName");
+		
+		//演示下载文件
+		String absolutePath = this.getServletContext().getRealPath(dir+fileName);
+		
+		System.out.println("文件绝对路径："+absolutePath);
+		
+		response.setHeader("Content-Disposition", "attachment;filename="+fileName);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doGet(request, response);
+	}
+
+}
